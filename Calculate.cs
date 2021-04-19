044e0ccb2c5e1552e4bf0b9f5f64bcce3964f27a
Silentassassin <s3lentassassin22@gmail.com>
2021-04-19 03:02:35 -0400

    Add project files.

---
+++ Calculate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculate
+    {
+        float num1;
+        float num2;
+        
+        private void SetNumber(float num)
+        {
+            if(ISno)
+        }
+    }
+}
---
+++ Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Math
+    {
+        public float Add(float i, float j)
+        {
+            return i + j;
+        }
+
+        public float Subtract(float i, float j)
+        {
+            return i - j;
+        }
+
+        public float Multiply(float i, float j)
+        {
+            return i * j;
+        }
+
+        public float Divide(float i, float j)
+        {
+            return i / j;
+        }
+
+    }
+}
